4384d39df34a803bb0e0cbf5b47a361890354e78
Crowdbotics <engineering@crowdbotics.com>
2023-01-16 09:59:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-morning-de-57201",
+  url: "https://withered-morning-de-57201.botics.co",
+  api: "https://withered-morning-de-57201.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
